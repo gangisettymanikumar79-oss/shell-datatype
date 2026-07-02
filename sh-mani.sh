7ea25f7503960733ecf256ed1f23c0b894349b9c
.email.com <gangisettymani98@gmail.com>
2026-07-02 16:21:27 +0530

    if i want create new file so it isnot change the file

--- sh-mani.sh
+++ sh-mani.sh
@@ -7,4 +7,7 @@ sum =$(($Number1+$Number2))
 echo "sum is :$sum"
 #array
 movies=("powerranger" "RRR" "pushpa") #index always starts from 0
- echo "movies area :${movies[@]}"
+ echo "first movies area :${movies[0]}"
+ echo "second movies area :${movies[1]}"
+ echo "five movies area :${movies[2]}"
+
