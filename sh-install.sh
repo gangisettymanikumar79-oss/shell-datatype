87e9d7529ffca55243a30504342689fb4df31e7b
.email.com <gangisettymani98@gmail.com>
2026-07-02 17:45:22 +0530

    if i want create new file so it isnot change the file

--- sh-install.sh
+++ sh-install.sh
@@ -8,7 +8,7 @@ if [ $USERID -ne 0 ]; then
 fi
  #echo " i am contiting....."
  echo "installing mysql"
- dnf install mysql-server -y
+ dnf install mysql -y
 if [ $? -ne 0 ]; then
  echo "installing mysql...........Falied"
 exit 1
