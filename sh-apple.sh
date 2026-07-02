90f449a0c5b44dba29ab4711d07551f5a6d15cbb
.email.com <gangisettymani98@gmail.com>
2026-07-02 16:33:12 +0530

    if i want create new file so it isnot change the file

--- sh-apple.sh
+++ sh-apple.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 Number1=10
 Number2=60
-if [$number -ge 10 ]; then
+if [ $number -ge 10 ]; then
  echo "please the number is greater  than 10"
-elif [$number -eq 10 ]; then
+elif [ $number -eq 10 ]; then
  echo "please the number is equal  than 10"
 else
  echo "please the number is less  than 10"
