7fe894afe15280042f37313a3382850a46802023
.email.com <gangisettymani98@gmail.com>
2026-07-03 15:47:27 +0530

    if i want another new file

--- sh-loops.sh
+++ sh-loops.sh
@@ -3,6 +3,11 @@ USERID=$(id -u)
 Log_DIR=/var/log/shell-script
 Log_FILE="$Log_DIR/$0.log"
 timestmp=$(date "+%y-%m-%d %H:%M:%S")
+Black=\e[30m
+Red =\e[31m
+Green=\e[32m
+Yellow=\e[33m
+Blue=\e[34m
 
 if [ $USERID -ne 0 ]; then
   echo "please run the script with root access"
@@ -11,22 +16,22 @@ fi
 
 validate(){
     if [ $2 -ne 0 ]; then
-        echo "$timestmp installing $1...........Failed"
+        echo "$timestmp [ERROR] installing $1 is ...........$B Failed $N "
         exit 1
     else
-        echo "$timestmp installing $1...........success"
+        echo "$timestmp [INFO] installing $1 is ...........$R success $N"
     fi
 }
 
 for package in "$@"
 do
-    echo "$timestmp checking $package"
+    echo "$timestmp [ERROR] checking $package"
     dnf list installed "$package" &>> $Log_FILE
     if [ $? -ne 0 ]; then
         echo "$timestmp installing $package"
         dnf install "$package" -y &>> $Log_FILE
         validate "$package" $?
     else
-        echo "$timestmp $package is already installed...........skipping"
+        echo "$timestmp [INFO] $package is already installed...........skipping"
     fi
 done
