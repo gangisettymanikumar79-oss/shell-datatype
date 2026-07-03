896b56251aed554adbc71b25095ba7d95584546f
.email.com <gangisettymani98@gmail.com>
2026-07-03 15:19:35 +0530

    if i want another new file

--- sh-loops.sh
+++ sh-loops.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
 USERID=$(id -u)
 Log_DIR=/var/log/shell-script
-Log_FILE="$Log_DIR/$0.log" #/home/ec2-user/shell-log.sh.log
-#correct root access or not
-if [ $USERID -ne 0 ]; then
-  echo "please the run script with root access"
-  exit 1
+Log_FILE="$Log_DIR/$0.log"
 
+if [ $USERID -ne 0 ]; then
+  echo "please run the script with root access"
+  exit 1
 fi
-## first arg -> what are you trying to install
-# second arg -> exit code
+
 validate(){
     if [ $2 -ne 0 ]; then
-       echo "installing $1...........Falied" 
-          exit 1
+        echo "installing $1...........Failed"
+        exit 1
     else
-        echo "installing $1...........success"  
+        echo "installing $1...........success"
     fi
-
 }
-for package in $@
+
+for package in "$@"
 do
-        echo "installing $package" 
-            dnf list installing $package &>> $Log_FILE
+    echo "checking $package"
+    dnf list installed "$package" &>> $Log_FILE
     if [ $? -ne 0 ]; then
-        dnf "$package installing -y &>> $Log_FILE
-            validate installing "$package" $?
+        echo "installing $package"
+        dnf install "$package" -y &>> $Log_FILE
+        validate "$package" $?
     else
-        echo "$package installing already...........skipping"
-           
+        echo "$package is already installed...........skipping"
     fi
 done
